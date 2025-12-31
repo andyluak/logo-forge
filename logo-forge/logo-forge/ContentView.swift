0ca24cfd84d9136cd48fab31a1238a107c65cde5
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2025-12-31 16:28:40 +0200

    feat(ui) - initial

---
+++ logo-forge/logo-forge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  logo-forge
+//
+//  Created by Alexandru Tirim on 31.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ logo-forge/logo-forge/logo_forgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  logo_forgeApp.swift
+//  logo-forge
+//
+//  Created by Alexandru Tirim on 31.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct logo_forgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
